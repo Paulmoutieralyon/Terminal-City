0e32f7887c4eae4d4d425b266314b214e0d3e1fa
Paul Moutier[LYON] <moutier.paul@gmail.com>
2018-09-05 21:25:45 +0200

    école

---
+++ City.sh
@@ -0,0 +1,110 @@
+   Mystère du cd
+    ls
+    cd ..
+  ls
+  instructions du chat 
+  ls
+    Mystère de 9 cd
+ ls
+  grep -i CLUE crimescene
+  entretiens cd
+  ls
+  têtes
+  interviews de tête
+   cd ..
+  ls
+  entretiens principaux
+   Interviews de  cd
+ interview
+    tête d'homme
+   cd ..
+   ls
+   membres cd
+   ls
+  chat AAA
+    cd ..
+    ls
+   personnes chat
+   personnes annabel grep
+    personnes annabel grep
+   personnes chat
+   personnes à Annabel
+   ls
+   cd rues
+   ls
+  chat hart Place
+   chat Hart_Place
+  ls
+ tête n 40
+ tête n-40
+   tête -n40
+  ls
+   cd ..
+   ls
+   Rues de  cd
+  tête -n40 Hart_Place
+  têtes -n179 Buckingham_Place
+  cd ..
+  ls
+  entretiens cd
+  ls
+  chat interview-47246024
+   chat interview-699607
+  cd ..
+  ls
+  véhicules de chat
+  véhicules "honda" grep
+   ls
+  véhicules grep honda
+   véhicules grep -i honda
+   véhicules de chat
+   véhicules grep -i L337
+   homme grep
+   véhicules grep -x L337
+   ls
+  véhicules grep -A "L337"
+   véhicules grep -x "L337"
+  véhicules grep "L337"
+   véhicules grep -p "L337"
+   véhicules grep-A "L337"
+   homme grep
+   véhicules grep -A 5 "L337"
+   ls
+  grep bower gens
+   grep -i embellit les gens
+   grep -i maher people
+   personnes de grep -i germuska
+   ls
+   Rues de 80 cd
+   ls
+   chat Dunstable_Road 
+   grep -i AAA Dunstable_Road 
+   grep -i musée Dunstable_Road 
+   grep -i AAA Walham_Street
+   grep -i AAA Waltham_Street
+   ls
+   grep -i AAA Plainfield_Street
+   chat Plainfield_Street
+   grep -i Delta 
+   grep -i delta Dunstable_Road
+   tête -n284 Dunstable_road
+   tête -n340 waltham_street
+  tête -n275 Plainfield_Street
+   cd ..
+  cd ..
+    ls
+    entretiens cd
+   ls
+   entrevue de chat-9620713
+   chat interview-29741223
+  ls
+ cd ..
+  ls
+  Adhésion au  cd
+ls
+ grep -i bowers AAA
+   grep -i bowers Delta_SkyMiles
+   grep -i bowers Museum
+   grep -i bowers Museum_of_Bash_History 
+ échos Jeremy Bowers
+ historique> mon_historique.log
